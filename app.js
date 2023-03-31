ea89984a251c01a4513345cb0254a80ddcb8c5d3
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-04-01 01:01:24 +0530

    changes

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require('express')
 const app = express()
 
-const { people } = require('./data')
+const { products } = require('./data')
 
 app.get('/', (req, res) => {
-  res.send('<h1>Click to view database data from data.js</h1><a href="/people">Click Here</a>')
-})
-app.get('/people',function(req, res){
-
-  const singleData = people.map(function(item){
-    const {id} = item
-    return({id})
-  })
-  res.json(singleData)
-
+  res.json(products)
 })
 
 app.listen(5000, () => {
