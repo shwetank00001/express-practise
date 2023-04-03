3ea3720b1ec64cddd5a675a73a237606b5538923
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-04-03 20:17:47 +0530

    route params practise

--- app.js
+++ app.js
@@ -1,58 +1,30 @@
 const express = require('express')
-const app = express()
-const { products } = require('./data')
+const app= express()
 
-app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+const {products} = require('./data')
+
+
+
+
+
+app.get('/', (req,res)=>{
+  res.send('Home Page! <a href= "/products">Click here </a><a href="/products/api">CLick for few Data</a>')
 })
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
+
+app.get('/products', function(req,res){
+  res.json(products)
+})
+
+app.get('/products/api', function(req,res){
+  const newProd = products.map(function(item){
+    const {id,name} = item
+    return({id,name})
   })
 
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productID } = req.params
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-  if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
-
-  return res.json(singleProduct)
+  res.send(newProd)
+  
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
-})
-
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
-  }
-  res.status(200).json(sortedProducts)
-})
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+app.listen(5000, ()=>{
+  console.log('AT port 5000')
 })
