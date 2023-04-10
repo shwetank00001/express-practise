716642be8c34980a4e5e00da4c887565d62448a1
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-04-10 23:32:48 +0500

    test

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const app = express()
 
-const people = require('./routes/people')
-const auth = require('./routes/auth')
+//  req => middleware => res
 
-// static assets
-app.use(express.static('./methods-public'))
-// parse form data
-app.use(express.urlencoded({ extended: false }))
-// parse json
-app.use(express.json())
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
 
-app.use('/api/people', people)
-app.use('/login', auth)
+app.get('/', logger, (req, res) => {
+  res.send('Home')
+})
+app.get('/about', logger, (req, res) => {
+  res.send('About')
+})
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
