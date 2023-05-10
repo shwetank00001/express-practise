b15f31fed5970a25f37fbf06ee77e3e856de1d90
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-05-11 03:01:12 +0530

    .

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ const express = require('express')
 const app = express()
 const {people} = require('./data')
 
-app.get('/', (req,res)=>{
-    res.status(200).json({success:"true",people})
+
+// static page using methods-public
+
+app.use(express.static('./methods-public'))
+
+// parse form data
+
+app.use(express.urlencoded({extemded:false}))
+
+
+
+// we use GET to read the data
+
+app.get('/api/people', (req,res)=>{
+    res.status(200).json({status:"OK",data:people})
 })
+
+app.post('/login', (req,res)=>{
+    console.log(req.body)
+    res.send("POST")
+})
+
+// how can we insert data on server ? We use POST method to send data
 
 app.listen(5000,(req,res)=>{
     console.log("APP ON PORT 5000")
