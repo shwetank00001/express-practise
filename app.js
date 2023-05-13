08060e5fb100b4a466a878c9175fa9d7a8c2348c
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-05-13 17:26:48 +0530

    working on req.body json objexts

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('./methods-public'))
 
 // parse form data
 
-app.use(express.urlencoded({extemded:false}))
+app.use(express.urlencoded({extended:false}))
 
 
 
@@ -19,14 +19,32 @@ app.get('/api/people', (req,res)=>{
     res.status(200).json({status:"OK",data:people})
 })
 
+// how can we insert data on server ? We use POST method to send data
+
 app.post('/login', (req,res)=>{
+
     console.log(req.body)
-    const {name, age} = req.body
+    const {name, year} = req.body
+    
     if(name){
-        return res.send(`Welcome ${name} ${age}`).status(200)
+        return res.send(`Welcome ${name} ${year}`).status(200)
     }
-    res.send("Add a name")
+    else{
+        res.send("Add a name")
+    }
+    
 })
+
+// In web development, req.body is an object that contains data submitted in the body of an HTTP POST request.
+
+// When a client sends an HTTP POST request to a server, the data submitted in the body of the request can be accessed on the server-side using req.body. This data can be in various formats such as JSON, XML, or plain text.
+
+// For example, if a user submits a form with fields like name, email, and message, the data entered in these fields will be sent in the body of the HTTP POST request. The server can then use req.body to access this data and process it accordingly.
+
+// In order to use req.body in a Node.js server, you typically need to use a middleware like body-parser to parse the request body before it can be accessed. This middleware can extract the submitted data and populate req.body with it.
+
+
+
 
 // how can we insert data on server ? We use POST method to send data
 
